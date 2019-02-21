f3037778a864fc208b45d7311f9a34a1e92424b5
antoine <antoine.guerra@epitech.eu>
2019-02-21 23:04:58 +0100

    add README + Put settings in modal

---
+++ settings.js
@@ -0,0 +1,3 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '889910005804-tq4btqe01v6sapk7fk65telp3kcdle3p.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyD42yhsKM9BM71lbkWy1LeX7m68oKxKCbw';
